a1966d9380fa75f47d3413360989e25fe9981fb3
Carlos Rosario <carlosrosario2k@gmail.com>
2020-06-23 22:35:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,32 @@
+require 'pry'
+
 class School
 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
 
-end
+  def roster
+    @roster
+  end
 
-school = School.new("Bayside High School")
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
